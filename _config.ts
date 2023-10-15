15da141579356f4683c4052e2d5375e4bbfb0927
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-10-15 09:39:09 +0300

    Remove unused integrations

--- _config.ts
+++ _config.ts
@@ -5,13 +5,12 @@ import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
-import netlifyCMS from "lume/plugins/netlify_cms.ts";
 import pageFind from "lume/plugins/pagefind.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import feed from "lume/plugins/feed.ts";
 
 const site = lume({
-  location: new URL("https://example.com/"),
+  location: new URL("https://joonasnuutinen.com/"),
 });
 
 site
@@ -41,6 +40,5 @@ site
     },
   }))
   .use(resolveUrls())
-  .use(netlifyCMS({ netlifyIdentity: true }));
 
 export default site;
